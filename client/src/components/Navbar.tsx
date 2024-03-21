dd2147ca4e5491d4af20c0b6f9886ea290d10e5d
WiseyXD <aryan.s.nag@gmail.com>
2024-03-21 11:14:36 +0530

    build : navbar done

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+import { Separator } from "@/components/ui/separator";
 
 export default function Navbar() {
-    return <div>Navbar</div>;
+    return (
+        <>
+            <div className="text-3xl pl-4 py-2">
+                <span className="text-red-600">tuf</span>Code
+            </div>
+            <Separator />
+        </>
+    );
 }
